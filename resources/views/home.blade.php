90b7645940824e321781887f42ae661fd6ae4aa6
anderjava <89351272+anderjava@users.noreply.github.com>
2021-09-24 23:52:21 -0600

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,7 +7,7 @@
 <div class="container">
     <div class="row">
         <div class="col-12 col-lg-6">
-            <h1 class="display-4 text-primary">Desarrollo Web EsauCode</h1>
+            <h1 class="display-4 text-primary">Desarrollo Web ANDERCOD</h1>
             <p class="lead text-secondary">gracias por visitar el sitio , espero te guste</p>
             <a class="btn btn-lg btn-block btn-primary" href="{{ route('contact') }}">Contactame</a>
             <a class="btn btn-lg btn-block btn-outline-primary" href="{{ route('projects.index') }}">portafolio</a>
